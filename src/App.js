ad3cd6f24ea8e222aee3c7f63297a102659cca54
HsuMonA <HsuMonA@users.noreply.github.com>
2023-01-28 17:25:53 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React from "react";
-import logo from "./logo.svg";
+import GoogleMapReact from "google-map-react";
+//import logo from "./logo.svg";
 import "./App.css";
+import Flat from "./components/flat";
+import Marker from "./components/marker";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      flats: [],
+    };
   }
+  componentDidMount() {
+    const url =
+      "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data,
+        });
+      });
+  }
+
   render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522,
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="main">
+          <div className="search"></div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />;
+            })}
+          </div>
+        </div>
+        <div className="map">
+          <GoogleMapReact center={center} zoom={11}>
+            {this.state.flats.map((flat) => {
+              return <Marker lat={flat.lat} lng={flat.lng} marker={flat.price} />;
+            })}
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//import Hello from "./components/hello";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//const root = <div>
+//    <Hello firstName="Boris" lastName="Paillard" />
+//    <Hello firstName="Boris" lastName="Paillard" />
+//  </div>;
+ReactDOM.render( <App />, document.getElementById("root"));
 
 reportWebVitals();
